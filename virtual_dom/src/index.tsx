17f7b10b937be21b0858d8ee29a33c932b992d34
migyeonglee <ak10044@naver.com>
2023-02-03 22:30:20 +0900

    virtual dom physical virtual DOM

--- virtual_dom/src/index.tsx
+++ virtual_dom/src/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// js만 사용하는 FE 개발 (물리DOM)
+let pPhysicalDOM = document.createElement('p')
+pPhysicalDOM.innerText = 'hello physical DOM world'
+document.body.appendChild(pPhysicalDOM)
+
+//가상 DOM
+
+const pVirtuaDOM = React.createElement('p', null, 'hello virtual DOM world')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(pVirtuaDOM)
